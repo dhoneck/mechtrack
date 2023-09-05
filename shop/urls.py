6525af91c3edfaebb740b30d04687879bbc362fc
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2023-09-04 21:27:57 -0500

    Changed to class-based views

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from shop import views
 
 urlpatterns = [
-    path('customers/', views.customer_list),
-    path('customers/<int:pk>/', views.customer_detail),
+    path('customers/', views.CustomerList.as_view()),
+    path('customers/<int:pk>/', views.CustomerDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from shop.models import Business, Branch, Car, Customer, Owner, Invoice
 from shop.serializers import (BusinessSerializer,
@@ -15,45 +11,51 @@ from shop.serializers import (BusinessSerializer,
 
 
 # Create your views here.
-@api_view(['GET', 'POST'])
-def customer_list(request, format=None):
+class CustomerList(APIView):
     """
     List all customers, or create a new customer.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def customer_detail(request, pk, format=None):
+class CustomerDetail(APIView):
     """
-    Retrieve, update or delete a customer.
+    Retrieve, update or delete a customer instance.
     """
-    try:
-        customer = Customer.objects.get(pk=pk)
-    except Customer.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Customer.objects.get(pk=pk)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        customer = self.get_object(pk)
         serializer = CustomerSerializer(customer)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = CustomerSerializer(customer, data=request.data)
+    def put(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        serializer = CustomerSerializer(customer,
+                                        data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        customer = self.get_object(pk)
         customer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
